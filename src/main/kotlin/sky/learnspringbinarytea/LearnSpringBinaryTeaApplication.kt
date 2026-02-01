4c4e5d4123e4a3ff1d63e7870b1f24e6f50a7289
skyloved <w2783858980@outlook.com>
2026-02-01 13:20:23 +0800

    initialize LearnSpring-BinaryTea project with basic structure and configuration

---
+++ src/main/kotlin/sky/learnspringbinarytea/LearnSpringBinaryTeaApplication.kt
@@ -0,0 +1,11 @@
+package sky.learnspringbinarytea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnSpringBinaryTeaApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnSpringBinaryTeaApplication>(*args)
+}
