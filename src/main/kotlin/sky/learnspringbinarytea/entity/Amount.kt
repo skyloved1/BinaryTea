2067244b5d82a98c6fe76c3c3edd31f2d93973e1
skyloved <w2783858980@outlook.com>
2026-03-01 16:14:01 +0800

    Refactor Amount class to use non-nullable properties for discount, totalAmount, and payAmount

--- src/main/kotlin/sky/learnspringbinarytea/entity/Amount.kt
+++ src/main/kotlin/sky/learnspringbinarytea/entity/Amount.kt
@@ -6,16 +6,17 @@ import jakarta.persistence.Embeddable
 import org.joda.money.Money
 
 @Embeddable
-class Amount(
+data class Amount(
     @Column(name = "amount_discount")
-    var discount: Int? = null,
+    var discount: Int? ,
 
     @Column(name = "amount_total")
     @Convert(converter = MoneyConverter::class)
-    var totalAmount: Money? = null,
+    var totalAmount: Money? ,
+
     @Column(name = "amount_pay")
     @Convert(converter = MoneyConverter::class)
-    var payAmount: Money?= null
+    var payAmount: Money?
 ) {
 
 
