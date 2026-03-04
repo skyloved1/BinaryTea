7a840bdb0686a30a983d25274d74a170bef98a26
skyloved <w2783858980@outlook.com>
2026-03-04 10:46:28 +0800

    Add Redis configuration and test setup; include Lettuce client customization

--- src/main/kotlin/sky/learnspringbinarytea/config/RedisConfiguration.kt
+++ src/main/kotlin/sky/learnspringbinarytea/config/RedisConfiguration.kt
@@ -1,8 +1,17 @@
 package sky.learnspringbinarytea.config
 
+import io.lettuce.core.ReadFrom
+import org.springframework.boot.data.redis.autoconfigure.LettuceClientConfigurationBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class RedisConfiguration {
+
+    @Bean
+    fun lettuceCustomizer() = LettuceClientConfigurationBuilderCustomizer {
+        configurationBuilder ->
+        configurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED)
+    }
+
 }
--- src/test/kotlin/sky/learnspringbinarytea/redis/RedisTest.kt
+++ src/test/kotlin/sky/learnspringbinarytea/redis/RedisTest.kt
@@ -1,12 +1,9 @@
-package sky.learnspringbinarytea
+package sky.learnspringbinarytea.redis
 
-import io.lettuce.core.RedisClient
-import org.springframework.boot.test.context.SpringBootTest
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.core.StringRedisTemplate
-import org.springframework.data.redis.core.convert.RedisData
 import kotlin.test.assertEquals
 
 @SpringBootTest
