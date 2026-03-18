5b9bd8f5c23b521cff096becd10d7ab1e0211fd2
skyloved <w2783858980@outlook.com>
2026-03-18 21:41:15 +0800

    Add 404 error page and update Redis test to use getAndDelete

--- src/main/kotlin/sky/learnspringbinarytea/config/CacheConfiguration.kt
+++ src/main/kotlin/sky/learnspringbinarytea/config/CacheConfiguration.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.web.filter.ShallowEtagHeaderFilter
 import sky.learnspringbinarytea.service.MenuService
 import sky.learnspringbinarytea.repository.MenuRepositoryByCrud
 import sky.learnspringbinarytea.repository.OrderRepository
@@ -27,4 +28,10 @@ class CacheConfiguration {
     fun orderCacheBean(orderRepository: OrderRepository)= OrderService(
         orderRepository = orderRepository
     )
+    ///本节中谈到的 ShallowEtagHeaderFilter 本质上并不会减少服务器端的计算工作 量，
+    // 所有的操作都会发生，它对发送给客户端的数据进行了额外的计算，产生一个 ETag，
+    // 如果这个值与客户端传过来的值一样，就不再传输内容，直接告诉客户端没有变化，
+    // 用以前 的就行。因此，它是用额外的服务端 CPU 时间优化了网络传输和客户端感受到的耗时。
+    @Bean
+    fun global()= ShallowEtagHeaderFilter()
 }
