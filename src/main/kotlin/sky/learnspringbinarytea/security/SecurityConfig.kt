5eb16e3620d1b6d16967e81f8575b2290570bafe
skyloved <w2783858980@outlook.com>
2026-03-23 20:37:35 +0800

    Add security configuration and user management; update application profiles and initialize user tables

---
+++ src/main/kotlin/sky/learnspringbinarytea/security/SecurityConfig.kt
@@ -0,0 +1,78 @@
+package sky.learnspringbinarytea.security
+
+import org.springframework.beans.factory.ObjectProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.dao.DuplicateKeyException
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { it.requestMatchers("/menu").permitAll() }
+            .authorizeHttpRequests { it.anyRequest().authenticated() }
+            .formLogin({
+                it.defaultSuccessUrl("/order")
+                it.failureUrl("/login")
+            })
+            .httpBasic(withDefaults())
+        return http.build()
+    }
+
+    // 这里的密码编码器是为了演示而设置的，实际项目中应该使用更安全的密码存储方式
+    /* @Bean
+     fun userDetailsService(passwordEncoderProvider: ObjectProvider<PasswordEncoder>): UserDetailsService {
+         val passwordEncoder = passwordEncoderProvider.getIfAvailable { passwordEncoder() }
+         val employee = User.builder()
+             .username("lilei")
+             .password("binarytea")
+             .passwordEncoder(passwordEncoder::encode)
+             .roles("EMPLOYEE")
+             .build()
+         return InMemoryUserDetailsManager(employee)
+     }
+ */
+    @Bean
+    fun jdbcUserDetailsService(
+        passwordEncoderProvider: ObjectProvider<PasswordEncoder>,
+        dataSource: DataSource
+    ): UserDetailsService {
+        val passwordEncoder = passwordEncoderProvider.getIfAvailable { passwordEncoder() }
+        val jdbcUserDetailsManager = JdbcUserDetailsManager(dataSource)
+        val employee = User.builder()
+            .username("lilei")
+            .password("binarytea")
+            .passwordEncoder(passwordEncoder::encode)
+            .roles("EMPLOYEE")
+            .build()
+        runCatching {
+            jdbcUserDetailsManager.createUser(employee)
+        }.onFailure {
+            // 如果用户已存在，可以忽略异常或打印日志
+            if (it is DuplicateKeyException)
+                logger.warn("User 'lilei' already exists in the database. Skipping user creation.")
+            else {
+                logger.error("Error creating user 'lilei': ${it.message}")
+                throw it
+            }
+        }
+        return jdbcUserDetailsManager
+    }
+}
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -11,3 +11,15 @@ create  table if not exists  t_demo(
                                        update_time timestamp,
                                        primary key (id)
 );
+
+
+drop table if exists users;
+drop table if exists authorities;
+create table users(
+                      username varchar(50) not null primary key, password varchar(500) not null,
+                      enabled boolean not null
+);
+create table authorities (
+                             username varchar(50) not null, authority varchar(50) not null
+);
+create unique index ix_auth_username on authorities (username, authority);
